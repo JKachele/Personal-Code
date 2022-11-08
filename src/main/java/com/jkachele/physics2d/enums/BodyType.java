03e54a462d779c6460d694ff78901e93d213fe3b
Justin Kachele <justin@kachele.com>
2022-11-08 17:09:54 -0500

    Added Physics Engine Code

---
+++ src/main/java/com/jkachele/physics2d/enums/BodyType.java
@@ -0,0 +1,14 @@
+/******************************************
+ *Project-------Engine2D-LWJGL
+ *File----------BodyType.java
+ *Author--------Justin Kachele
+ *Date----------11/8/2022
+ *License-------MIT License
+ ******************************************/
+package com.jkachele.game.physics2dtmp.enums;
+
+public enum BodyType {
+    STATIC,
+    KINEMATIC,
+    DYNAMIC
+}
